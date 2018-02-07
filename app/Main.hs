0ed2b8348e0ec1825c0929b8dc42ee090a82cbc0
Ludvig Sundström <lud.sund@gmail.com>
2018-02-07 14:51:01 +0100

    Standard config

--- app/Main.hs
+++ app/Main.hs
@@ -1,37 +1,18 @@
 module Main where
 
 import  Protolude            hiding (filter)
-import  Data.Text            as T   (unpack, filter)
-import  Data.List            hiding (filter)
+import  Data.Text            as T   (unpack)
 
 -- import Analyze
-import  LazyTree.Functional
 -- import  Draw
 import  Search
 
--------------------------------------------------------------------------------
--- Program
-
-removeNewLines :: Text -> Text
-removeNewLines = filter (/= '\n')
-
-treeFromFile :: FilePath -> IO (STree Char)
-treeFromFile path = do
-    cont <- readFile path
-    let s = unpack (removeNewLines cont)
-    print s
-    return $ lazyCST (nub s) s
-
-
--- abaababa
 main :: IO ()
 main =
     do
-    cont1 <- readFile "data/book/data.xml"
+    text <- readFile "data/book/data.xml"
     alpha <- readFile "data/book/alpha.txt"
-    -- t <- treeFromFile "data/advAlg"
-    -- drawPretty t
-    -- cont <- readFile "data/advAlg"
-    print $ indices (unpack alpha) (unpack cont1) "ENTSCHEIDUNG"
-
-
+    let alphaS = unpack alpha
+        textS  = unpack text
+        pattern = "ABI. L 185, S. 5"
+    print $ indices alphaS textS pattern
