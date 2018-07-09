d4553ae9c3c10c8de52a2c92abd1c4e54e8bb699
Ludvig Sundström <lud.sund@gmail.com>
2018-07-09 19:29:57 +0200

    Modify Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,24 @@
 module Main where
 
-import qualified Data.Text.Lazy                           as T
-import           Data.Text.Lazy.IO                        (readFile, appendFile, writeFile)
-import           Protolude                                hiding (Text,
-                                                           readFile, appendFile, writeFile)
+import qualified Data.Text.Lazy                         as T
+import           Data.Text.Lazy.IO                      (appendFile, readFile,
+                                                         writeFile)
+import           Protolude                              hiding (Text,
+                                                         appendFile, readFile,
+                                                         writeFile)
 
 import           SuffixTree.Algorithm.LazyTreeCountSort
 import           SuffixTree.Algorithm.Search
 
+-- Specify input file here
 datafile :: FilePath
-datafile = "data/book/data.xml"
-alphafile :: FilePath
-alphafile = "data/book/alpha.txt"
+datafile = "data.txt"
 
 main :: IO ()
 main = do
     content <- readFile datafile
     let nos = T.lines content
         tree = lazyTreeCount content
-    print "todo"
+    print "Done."
 
 
